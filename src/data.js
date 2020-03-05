1b15720a8d124fae8d7dcfc8f9bc524f58859c3d
vanyaxel <vanya_xel@live.com.mx>
2020-03-05 12:35:09 -0600

    css modificaciones

--- src/data.js
+++ src/data.js
@@ -1,3 +1,49 @@
+export const poke = {
+    dataPokemon : function (){ 
+
+let section = document.querySelector("#listPokemon");
+
+let requestURL= "./pokemon.json"; // url esta bien?
+let request = new XMLHttpRequest(); // probar sin este, por que no tenemos que jalar info de un servidor 
+request.open("GET", requestURL);
+request.responseType ="json";
+request.send();
+
+request.onload = function(){
+    let cataloguePokemon = request.response;
+    showPokemon (cataloguePokemon); 
+}
+
+function showPokemon (jsonObj){
+    let pokemones = jsonObj["pokemon"];
+
+    for (let i= 0; i< pokemones.length; i++){
+        let myArticle = document.createElement("article");
+        let myH2 = document.createElement("h2");
+        let myPara1 = document.createElement("p");
+        let myPara2 = document.createElement("p");
+        let myPara3 = document.createElement("p");
+
+
+        myH2.textContent = pokemones[i].name; //name?
+        myPara1.textContent = "Tipo: " + pokemones[i].type;
+        myPara2.textContent = "Numero de Pokemón " + pokemones[i].num;
+        myPara3.textContent = "Id: " + pokemones[i].id;
+
+        myArticle.appendChild(myH2);
+        myArticle.appendChild(myPara1);
+        myArticle.appendChild(myPara2);
+        myArticle.appendChild(myPara3);
+
+        section.appendChild(myArticle);
+    }
+  }
+   }
+} 
+
+
+
+
 // import data from './data/pokemon/pokemon.js'
 // esta es una función de ejemplo
 /*
@@ -43,41 +89,3 @@ function showPokemon (jsonObj){
     }
   }
 */
-
-var section = document.querySelector("section");
-
-var requestURL= "./pokemon.js"; // url esta bien?
-var request = new XMLHttpRequest(); // probar sin este, por que no tenemos que jalar info de un servidor 
-request.open("GET", requestURL);
-request.responseType ="json";
-request.send();
-
-request.onload = function(){
-    var cataloguePokemon = request.response;
-    showPokemon (cataloguePokemon); 
-}
-
-function showPokemon (jsonObj){
-    var pokemones = jsonObj["pokemon"];
-
-    for (var i= 0; i< pokemones.length; i++){
-        var myArticle = document.createElement("article");
-        var myH2 = document.createElement("h2");
-        var myPara1 = document.createElement("p");
-        var myPara2 = document.createElement("p");
-        var myPara3 = document.createElement("p");
-
-
-        myH2.textContent = pokemones[i].name; //name?
-        myPara1.textContent = "Tipo: " + pokemones[i].type;
-        myPara2.textContent = "Numero de Pokemón " + pokemones[i].num;
-        myPara3.textContent = "Id: " + pokemones[i].id;
-
-        myArticle.appendChild(myH2);
-        myArticle.appendChild(myPara1);
-        myArticle.appendChild(myPara2);
-        myArticle.appendChild(myPara3);
-
-        section.appendChild(myArticle);
-    }
-  }
--- src/main.js
+++ src/main.js
@@ -1,14 +1,26 @@
-import  pokemon  from './pokemon.js';
 
-const pokemonArr = pokemon 
+import   { poke }  from "./data.js";
+let data = './pokemon.json'
+/* const pokemonArr = pokemon 
 const pokemons= pokemon.pokemon 
 pokemons.map(pokes =>console.log(pokes.pokemon));
+ */
 
 
+friend.onclick = function(){
+    document.getElementById("listPokemon").value = poke.showPokemon (data);
+    document.getElementById("listPokemon").innerHTML = poke.showPokemon (data);
+}
+ 
+
+
+
+
+/* 
 document.getElementById('log-in').addEventListener('click',()=>{
     document.getElementById('sign-in').style.display = 'none';
     document.getElementById('menu').style.display = 'block';
     document.getElementById('list').style.display = 'none'; 
     document.getElementById('pokemon-centers').style.display = 'none';
     document.getElementById('pokemon-file').style.display = 'none';
-}
+}) */
