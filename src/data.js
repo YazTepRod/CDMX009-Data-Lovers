d44060f9ba4bf684c1175c2e42c5bc2b93e20026
vanyaxel <vanya_xel@live.com.mx>
2020-03-05 09:29:37 -0600

    css y js

--- src/data.js
+++ src/data.js
@@ -2,7 +2,84 @@
 
 
 // esta es una función de ejemplo
-
+/*
 export const example = () => {
   return 'example';
-};
+};*/
+/*
+var section = document.querySelector("section");
+
+var requestURL= "./pokemon.json"; // url esta bien?
+var request = new XMLHttpRequest(); // probar sin este, por que no tenemos que jalar info de un servidor.
+request.open("GET", requestURL);
+request.responseType ="json";
+request.send();
+
+request.onload = function(){
+    var cataloguePokemon = request.response;
+    showPokemon (cataloguePokemon); 
+}
+
+function showPokemon (jsonObj){
+    var pokemones = jsonObj["pokemon"];
+
+    for (var i= 0; i< pokemones.length; i++){
+        var myArticle = document.createElement("article");
+        var myH2 = document.createElement("h2");
+        var myPara1 = document.createElement("p");
+        var myPara2 = document.createElement("p");
+        var myPara3 = document.createElement("p");
+
+
+        myH2.textContent = pokemones[i].name; //name?
+        myPara1.textContent = "Tipo: " + pokemones[i].type;
+        myPara2.textContent = "Numero de Pokemón " + pokemones[i].num;
+        myPara3.textContent = "Id: " + pokemones[i].id;
+
+        myArticle.appendChild(myH2);
+        myArticle.appendChild(myPara1);
+        myArticle.appendChild(myPara2);
+        myArticle.appendChild(myPara3);
+
+        section.appendChild(myArticle);
+    }
+  }
+*/
+
+var section = document.querySelector("section");
+
+var requestURL= "./pokemon.js"; // url esta bien?
+var request = new XMLHttpRequest(); // probar sin este, por que no tenemos que jalar info de un servidor 
+request.open("GET", requestURL);
+request.responseType ="json";
+request.send();
+
+request.onload = function(){
+    var cataloguePokemon = request.response;
+    showPokemon (cataloguePokemon); 
+}
+
+function showPokemon (jsonObj){
+    var pokemones = jsonObj["pokemon"];
+
+    for (var i= 0; i< pokemones.length; i++){
+        var myArticle = document.createElement("article");
+        var myH2 = document.createElement("h2");
+        var myPara1 = document.createElement("p");
+        var myPara2 = document.createElement("p");
+        var myPara3 = document.createElement("p");
+
+
+        myH2.textContent = pokemones[i].name; //name?
+        myPara1.textContent = "Tipo: " + pokemones[i].type;
+        myPara2.textContent = "Numero de Pokemón " + pokemones[i].num;
+        myPara3.textContent = "Id: " + pokemones[i].id;
+
+        myArticle.appendChild(myH2);
+        myArticle.appendChild(myPara1);
+        myArticle.appendChild(myPara2);
+        myArticle.appendChild(myPara3);
+
+        section.appendChild(myArticle);
+    }
+  }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-import { pokemon } from './pokemon.js';
+import  pokemon  from './pokemon.js';
 
-
-
-
+const pokemonArr = pokemon 
+const pokemons= pokemon.pokemon 
+pokemons.map(pokes =>console.log(pokes.pokemon));
 
 console.log(pokemon);
+
+document.getElementById('log-in').addEventListener('click',()=>{
+    document.getElementById('sign-in').style.display = 'none';
+    document.getElementById('menu').style.display = 'block';
+    document.getElementById('list').style.display = 'none'; 
+    document.getElementById('pokemon-centers').style.display = 'none';
+    document.getElementById('pokemon-file').style.display = 'none';
+    
 
